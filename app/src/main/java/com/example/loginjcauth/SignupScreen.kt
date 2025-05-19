b725e566cd0e2b53dce1b03c60650f0e80a8d10b
Owen-Yator <yatorowen@gmail.com>
2025-05-19 11:17:09 +0300

    Updates to the SignupScreen

--- app/src/main/java/com/example/loginjcauth/SignupScreen.kt
+++ app/src/main/java/com/example/loginjcauth/SignupScreen.kt
@@ -1,12 +1,16 @@
 package com.example.loginjcauth
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,8 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.google.firebase.auth.auth
 
 @Composable
 fun SignupScreen(navHostController: NavHostController){
@@ -28,6 +34,37 @@ fun SignupScreen(navHostController: NavHostController){
         Column(horizontalAlignment = Alignment.CenterHorizontally){
             Text("Sign Up", style = MaterialTheme.typography.headlineMedium)
             Spacer(modifier = Modifier.height(24.dp))
+
+            OutlinedTextField(
+                value = email,
+                onValueChange = {email = it},
+                label = {Text(email)},
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = {password = it},
+                label = {Text(password)},
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            val context = LocalContext.current
+
+            Button(onClick = {
+                Firebase.auth.createUserWithEmailandPassword(email, password).addOnCompleteListerner{
+                    task -> if (task.isSuccessful){
+                        Toast.makeText(context, "SignUp Successful! Please log in.", Toast.LENGTH_SHORT).show()
+                    navHostController.navigate("login")
+
+                }
+                }
+            }) { }
+
         }
     }
 
