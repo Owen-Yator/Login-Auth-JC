8b44446e7c4706f38f26109a8fb570535c52b2f8
Owen-Yator <yatorowen@gmail.com>
2025-05-21 08:59:41 +0300

    Updates to the build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose.jvmstubs)
     implementation(libs.androidx.runtime.android)
+    implementation(libs.firebase.common.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -60,4 +61,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("com.google.firebase:firebase-auth-ktx:23.2.0")
 }
